8c35cb09bc2b5c0ee907c65390367f360373bba2
MauMG03 <mauricio.munoz.gutierrez@correounivalle.edu.co>
2023-09-01 16:15:01 -0500

    figuras añadidas

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -3,19 +3,34 @@ import { useFrame } from "@react-three/fiber";
 import { useRef } from "react";
 
 const Experience = () => {
-    const boxRef = useRef(null);
+    const torusRef = useRef(null);
 
     useFrame((state,delta) => {
-        
+        torusRef.current.rotation.x += delta
     });
     return(
         <>
             <OrbitControls makeDefault/>
             <ambientLight intensity={0.5} />
             <directionalLight position={[10, 10, 5]} intensity={2} />
-            <mesh ref={boxRef}>
+            <mesh position={[0,0,0]}>
                 <boxGeometry args={[1,1,1]}/>    
                 <meshStandardMaterial color="yellow"/>
+            </mesh>
+
+            <mesh position={[-1,0,0]}>
+                <coneGeometry args={[0.5,1.5]}/>    
+                <meshMatcapMaterial color="green"/>
+            </mesh>
+
+            <mesh ref={torusRef} position={[0,-1,0]}>
+                <torusGeometry args={[4, 1, 30, 100]}/>    
+                <meshNormalMaterial/>
+            </mesh>
+
+            <mesh position={[1,0,0]}>
+                <sphereGeometry args={[0.5,64,20]}/>    
+                <meshToonMaterial color="red"/>
             </mesh>
         </>
     );
