052d8be29f4e0b8b8ebfd9d70502237bb056072b
MauMG03 <mauricio.munoz.gutierrez@correounivalle.edu.co>
2023-09-10 11:11:22 -0500

    Info component modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,18 @@
 import { Canvas } from "@react-three/fiber"
 import Experience from "./Experience"
-import Info from "./Info"
+import Info from "./Info/Info.jsx"
 import "./styles.css"
 
 const App = () => {
     return (
-        <div style={{ display: 'grid', gridTemplateColumns: '1fr', gridTemplateRows: '1fr', width: '100vw', height: '100vh' }}>
-            <div className="info">
-                <Info name="Mauricio Munoz" bio="3D web developer"/>
-            </div>
+        <>
+            <Info name={"Mauricio Munoz"} biography={"Web 3D Developer"}/>
             <Canvas
                 camera={{ position: [2, 0, 5] }}
             >
                 <Experience />
             </Canvas>
-            
-        </div>
+        </>
     )
 }
 export default App
---
+++ src/Info/Info.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./info.css"
+
+const Info = ({name,biography}) => {
+  return (
+    <div className="container-info">
+            <div className="card-info">
+                <h1 className="name">{name}</h1>
+                <span className="biography">{biography}</span>
+            </div>
+    </div>
+  )
+}
+
+export default Info
