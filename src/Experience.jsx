4868b34fdd66cae0220502eb3ef2d14f9b92994e
MauMG03 <mauricio.munoz.gutierrez@correounivalle.edu.co>
2023-09-19 23:54:06 -0500

    piso añadido

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -12,6 +12,10 @@ const Experience = () => {
             <directionalLight position={[10, 10, 5]} intensity={2} />
             <Desk/>
             <Computer rotate={[0,100,0]}/>
+            <mesh position-y={-4.8} position-x={2} rotation-x={-Math.PI / 2} >
+                <planeGeometry attach="geometry" args={[25, 25]}/>
+                <meshStandardMaterial attach="material" color="grey" />
+            </mesh>
         </>
     );
 }
